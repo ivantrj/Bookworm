9d2f0bf9d0f486a4acefb47e848d3220f43ac1c0
Ivan Trj <ivantrajanovski@gmail.com>
2023-07-02 11:21:47 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Ivan Trajanovski on 02.07.23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
