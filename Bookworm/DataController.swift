1d43fec5d1c55a6f8526f3db5523e9930345dcf9
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-07-10 14:36:07 +0200

    create data model, add view and home view

---
+++ Bookworm/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  Data-Controller.swift
+//  Bookworm
+//
+//  Created by ivan ruwido  on 10.07.23.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
